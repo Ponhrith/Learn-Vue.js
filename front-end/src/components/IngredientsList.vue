8b6d6efc6051e81ea02202625481d1a3d6857dbd
Ponhrith <ponhrith.nhep@gmail.com>
2023-03-05 23:09:34 +0700

    Testing

---
+++ front-end/src/components/IngredientsList.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="listcontainer">
+        <h3>Ingredients</h3>
+        <!--Display Ingredients List-->
+        <div v-for="ingredient in ingredients" :key="ingredient.name" class="list-item">
+            <h3>{{ ingredient.name }}</h3>
+            <p>{{ ingredient.name }} {{ ingredeint.units }}</p>
+        </div>
+        <router-link to="/add-ingredient">
+            <button>+ Add Ingredient</button>
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'IngredientsList',
+    props: ['ingredients'],
+
+}
+</script>
