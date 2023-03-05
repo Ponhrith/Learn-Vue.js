dbefcc773c6f8eafe32fc7879a18aabc97a1724c
Ponhrith <ponhrith.nhep@gmail.com>
2023-03-05 17:22:21 +0700

    Testing

---
+++ front-end/src/pages/RecipeSearchPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>This is the Recipe Search Page</h1>
+</template>
+
+<script>
+export default{
+    name: 'RecipeSearchPage',
+}
+</script>
---
+++ front-end/src/pages/ShoppingListPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>This is the Shopping List Page</h1>
+</template>
+
+<script>
+export default {
+    name: 'ShoppingListPage',
+}   
+</script>
